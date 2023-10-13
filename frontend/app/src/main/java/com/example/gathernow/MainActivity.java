38d9afa9918aa331089e938efa5a50e3ad32619a
민키 <minkhi@snu.ac.kr>
2023-10-13 22:04:43 +0900

    initial signUp, searchHome

--- frontend/app/src/main/java/com/example/gathernow/MainActivity.java
+++ frontend/app/src/main/java/com/example/gathernow/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gathernow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TO-DO
+        // implement log in page
+        Button button2 = (Button) findViewById(R.id.myButton2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), signUp.class);
+                startActivity(intent);
+            } });
     }
+
+
+
 }
