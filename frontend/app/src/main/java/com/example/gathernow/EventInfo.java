31df1f4c7fa7516c090ec59c5d2cbdbc735a4755
Minh Luong <62457484+minhlgdo@users.noreply.github.com>
2023-10-26 15:39:05 +0900

    branch init

---
+++ frontend/app/src/main/java/com/example/gathernow/EventInfo.java
@@ -0,0 +1,22 @@
+package com.example.gathernow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class EventInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_info);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+
+    }
+}
