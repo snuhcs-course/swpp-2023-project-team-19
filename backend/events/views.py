991902f326b0ec82178ca1217515fa2c50e120ec
Minh Luong <62457484+minhlgdo@users.noreply.github.com>
2023-11-01 20:37:22 +0900

    feat: update back-end code to query events by event id

--- backend/events/views.py
+++ backend/events/views.py
@@ -18,6 +18,7 @@ def event_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Get a list of events
 @api_view(['GET', 'PUT', 'DELETE'])
 def event_detail(request, pk):
     # Retrieve, update or delete an event
@@ -47,6 +48,15 @@ def event_detail(request, pk):
 def events_by_user(request, user_id):
     if request.method == 'GET':
         events = Event.objects.filter(host_id=user_id)
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+# Get events by id
+@api_view(['GET'])
+def events_by_id(request, event_id):
+    if request.method == 'GET':
+        events = Event.objects.filter(event_id=event_id)
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
