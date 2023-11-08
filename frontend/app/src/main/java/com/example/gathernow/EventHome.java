30a42b6b9dc9b372e122a860b6ebd97af2ff5645
이치이 <50654006+cheeyee22@users.noreply.github.com>
2023-11-08 16:33:56 +0900

    Update EventHome.java

--- frontend/app/src/main/java/com/example/gathernow/EventHome.java
+++ frontend/app/src/main/java/com/example/gathernow/EventHome.java
@@ -202,7 +202,7 @@ public class EventHome extends Fragment {
 
                     EventCardView newEventCard = new EventCardView(getContext(), null);
                     newEventCard.setEventName(currentEvent.event_title);
-                    newEventCard.setEventPhoto(currentEvent.event_type);
+                    newEventCard.setEventPhoto(currentEvent.event_images, currentEvent.event_type);
                     newEventCard.setEventCapacity(currentEvent.event_num_joined, currentEvent.event_num_participants);
                     newEventCard.setEventLocation(currentEvent.event_location);
                     newEventCard.setEventLanguage(currentEvent.event_language);
