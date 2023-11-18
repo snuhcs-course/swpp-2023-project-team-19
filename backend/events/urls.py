65ecbf475c96cacf414ed6c95d219e8e0d5c8d9e
이치이 <50654006+cheeyee22@users.noreply.github.com>
2023-11-18 23:25:31 +0900

    feat: implement filter and search event function

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('events/one/<int:pk>/', views.event_detail, name='event-detail'),
     path('events/by_id/<int:event_id>/', views.events_by_id, name='events-by-id'),
     path('events/num_joined/add/<int:event_id>/', views.increase_num_joined, name='events-by-id'),
-    path('events/num_joined/delete/<int:event_id>/', views.decrease_num_joined, name='events-by-id')
+    path('events/num_joined/delete/<int:event_id>/', views.decrease_num_joined, name='events-by-id'),
+    path('events/filter', views.events_filter, name='events-filter'),
+    path('events/search', views.events_search, name='events-filter')
 ]
--- backend/events/views.py
+++ backend/events/views.py
@@ -6,10 +6,16 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Event
 from .serializers import EventSerializer
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.db.models import ExpressionWrapper, F, DateTimeField, Func, Value, CharField
 from django.db.models.functions import Concat
 from django.utils.timezone import make_aware
+from django.utils import timezone
+from datetime import time
+
+from django.shortcuts import render
+from django.db.models import Q
+from .models import Event
 
 @api_view(['GET', 'POST'])
 def event_list(request):
@@ -158,6 +164,7 @@ def increase_num_joined(request, event_id):
         event.save()
         serializer = EventSerializer(event)
         return Response(serializer.data)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
     
 @api_view(['PUT'])
 def decrease_num_joined(request, event_id):
@@ -171,3 +178,116 @@ def decrease_num_joined(request, event_id):
         event.save()
         serializer = EventSerializer(event)
         return Response(serializer.data)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def events_filter(request):
+    if request.method == 'GET':
+        # Extracting filter parameters from the GET request
+        languages = request.GET.getlist('language')  # Returns a list of languages
+        event_types = request.GET.getlist('event_type')  # Returns a list of event types
+        dates = request.GET.getlist('date')  # Returns a list of dates
+        times = request.GET.getlist('time')  # Returns a list of times
+        is_free = request.GET.get('is_free')  # Returns 'true' or 'false' as a string
+
+        now = make_aware(datetime.now())
+
+        # Annotate queryset with a combined datetime field
+        events = Event.objects.all().annotate(
+            full_event_datetime=ExpressionWrapper(
+                Func(
+                    Concat(
+                        F('event_date'), 
+                        Value(' '),  # Space to separate date and time
+                        F('event_time')
+                    ),
+                    function='CAST',
+                    template='%(function)s(%(expressions)s as datetime)',
+                ),
+                output_field=DateTimeField()
+            )
+        )
+
+        # Filter events happening after the current datetime
+        queryset = events.filter(full_event_datetime__gt=now)
+
+        # Applying language filter
+        if languages:
+            for language in languages:
+                queryset = queryset.filter(event_language__icontains=language)
+
+        # Applying event type filter
+        if event_types:
+            queryset = queryset.filter(event_type__in=event_types)
+
+        # Applying date filter
+        if dates:
+            today = timezone.now().date()
+            one_week_later = today + timedelta(days=7)
+            eight_day = today + timedelta(days=8)
+            two_week_later = today + timedelta(days=14)
+        
+            for date in dates:
+                if (date == "This week"):
+                    queryset = queryset.filter(event_date__range=(today, one_week_later))
+                elif(date == "Next week"):
+                    queryset = queryset.filter(event_date__range=(eight_day, two_week_later))
+                else:
+                    queryset = queryset.exclude(event_date__range=(today, two_week_later))
+
+        # Applying time filter
+        if times:
+            for time_period in times:
+                if (time_period == "Morning"):
+                    queryset = queryset.filter(event_time__gte=time(5, 0, 0), event_time__lt=time(11, 59, 59))
+                elif (time_period == "Afternoon"):
+                    queryset = queryset.filter(event_time__gte=time(12, 0, 0), event_time__lt=time(18, 59, 59))
+                else:
+                    queryset = queryset.exclude(event_time__gte=time(5, 0, 0), event_time__lt=time(11, 59, 59))
+                    queryset = queryset.exclude(event_time__gte=time(12, 0, 0), event_time__lt=time(18, 59, 59))
+
+        # Applying price filter (assuming 'price' field exists in your model)
+        if is_free:
+            queryset = queryset.filter(event_price=0)
+        else:
+            queryset = queryset.exclude(event_price=0)
+
+        # Serializing the data
+        serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def events_search(request):
+    if request.method == 'GET':
+        now = make_aware(datetime.now())
+
+        # Annotate queryset with a combined datetime field
+        events = Event.objects.all().annotate(
+            full_event_datetime=ExpressionWrapper(
+                Func(
+                    Concat(
+                        F('event_date'), 
+                        Value(' '),  # Space to separate date and time
+                        F('event_time')
+                    ),
+                    function='CAST',
+                    template='%(function)s(%(expressions)s as datetime)',
+                ),
+                output_field=DateTimeField()
+            )
+        )
+
+        # Filter events happening after the current datetime
+        queryset = events.filter(full_event_datetime__gt=now)
+
+        # Retrieve the search keyword from the request
+        search_keyword = request.GET.get('search', '')
+
+        # Filter the queryset based on the search keyword
+        if search_keyword:
+            queryset = queryset.filter(event_title__icontains=search_keyword)
+
+        serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
