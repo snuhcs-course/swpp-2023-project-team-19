43f151ac61dee6f3a035843d716511d345f295e7
이치이 <50654006+cheeyee22@users.noreply.github.com>
2023-11-18 20:21:27 +0900

    Update views.py

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,15 +1,43 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Event
 from .serializers import EventSerializer
+
+from datetime import datetime
+from django.db.models import ExpressionWrapper, F, DateTimeField, Func, Value, CharField
+from django.db.models.functions import Concat
+from django.utils.timezone import make_aware
 
 @api_view(['GET', 'POST'])
 def event_list(request):
     # List all events or create a new one
     if request.method == 'GET':
-        events = Event.objects.all()
-        serializer = EventSerializer(events, many=True)
+        # Get current datetime in an aware format (considering timezone)
+        now = make_aware(datetime.now())
+
+        # Annotate queryset with a combined datetime field
+        events = Event.objects.all().annotate(
+            full_event_datetime=ExpressionWrapper(
+                Func(
+                    Concat(
+                        F('event_date'), 
+                        Value(' '),  # Space to separate date and time
+                        F('event_time')
+                    ),
+                    function='CAST',
+                    template='%(function)s(%(expressions)s as datetime)',
+                ),
+                output_field=DateTimeField()
+            )
+        )
+        # Filter events happening after the current datetime
+        future_events = events.filter(full_event_datetime__gt=now)
+
+        # Serialize and return the filtered events
+        serializer = EventSerializer(future_events, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = EventSerializer(data=request.data)
@@ -21,6 +49,38 @@ def event_list(request):
                 event.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+#@permission_classes([IsAuthenticated])  # Optional
+def events_by_user(request, user_id):
+    if request.method == 'GET':
+        # Get current datetime in an aware format (considering timezone)
+        now = make_aware(datetime.now())
+
+        # Annotate queryset with a combined datetime field
+        events = Event.objects.filter(host_id=user_id).annotate(
+            full_event_datetime=ExpressionWrapper(
+                Func(
+                    Concat(
+                        F('event_date'), 
+                        Value(' '),  # Space to separate date and time
+                        F('event_time')
+                    ),
+                    function='CAST',
+                    template='%(function)s(%(expressions)s as datetime)',
+                ),
+                output_field=DateTimeField()
+            )
+        )
+
+        # Filter events happening after the current datetime
+        future_events = events.filter(full_event_datetime__gt=now)
+
+        # Serialize and return the filtered events
+        serializer = EventSerializer(future_events, many=True)
+        return Response(serializer.data)
+
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 # Get a list of events
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -48,12 +108,35 @@ def event_detail(request, pk):
 
 
 @api_view(['GET'])
-#@permission_classes([IsAuthenticated])  # Optional: If you want to protect the endpoint
+#@permission_classes([IsAuthenticated])  # Optional
 def events_by_user(request, user_id):
     if request.method == 'GET':
-        events = Event.objects.filter(host_id=user_id)
-        serializer = EventSerializer(events, many=True)
+        # Get current datetime in an aware format (considering timezone)
+        now = make_aware(datetime.now())
+
+        # Annotate queryset with a combined datetime field
+        events = Event.objects.filter(host_id=user_id).annotate(
+            full_event_datetime=ExpressionWrapper(
+                Func(
+                    Concat(
+                        F('event_date'), 
+                        Value(' '),  # Space to separate date and time
+                        F('event_time')
+                    ),
+                    function='CAST',
+                    template='%(function)s(%(expressions)s as datetime)',
+                ),
+                output_field=DateTimeField()
+            )
+        )
+
+        # Filter events happening after the current datetime
+        future_events = events.filter(full_event_datetime__gt=now)
+
+        # Serialize and return the filtered events
+        serializer = EventSerializer(future_events, many=True)
         return Response(serializer.data)
+
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 # Get & delete events by id
@@ -61,9 +144,65 @@ def events_by_user(request, user_id):
 def events_by_id(request, event_id):
     events = Event.objects.filter(event_id=event_id)
     if request.method == 'GET':
-        serializer = EventSerializer(events, many=True)
+
+        now = make_aware(datetime.now())
+
+        # Annotate queryset with a combined datetime field
+        events = Event.objects.filter(event_id = event_id).annotate(
+            full_event_datetime=ExpressionWrapper(
+                Func(
+                    Concat(
+                        F('event_date'), 
+                        Value(' '),  # Space to separate date and time
+                        F('event_time')
+                    ),
+                    function='CAST',
+                    template='%(function)s(%(expressions)s as datetime)',
+                ),
+                output_field=DateTimeField()
+            )
+        )
+
+        # Filter events happening after the current datetime
+        future_events = events.filter(full_event_datetime__gt=now)
+
+        if not future_events.exists():
+            # If there are no future events, return 204 No Content
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        # Serialize and return the filtered events
+        serializer = EventSerializer(future_events, many=True)
+
         return Response(serializer.data)
     elif request.method == 'DELETE':
         events.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+# Update request status in application
+@api_view(['PUT'])
+def increase_num_joined(request, event_id):
+    try:
+        event = Event.objects.get(event_id=event_id)
+    except Event.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        event.event_num_joined += 1
+        event.save()
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+    
+@api_view(['PUT'])
+def decrease_num_joined(request, event_id):
+    try:
+        event = Event.objects.get(event_id=event_id)
+    except Event.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        event.event_num_joined -= 1
+        event.save()
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
