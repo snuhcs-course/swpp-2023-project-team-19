657e07cfa0075aec729fad72bba16e6a3a4b1199
이치이 <50654006+cheeyee22@users.noreply.github.com>
2023-12-03 16:37:27 +0900

    debug: add import toast

--- frontend/app/src/main/java/com/example/gathernow/MainActivity.java
+++ frontend/app/src/main/java/com/example/gathernow/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gathernow;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
